c15d866807a3b03ba64cc36506877beb2970c62c
CRJL1001 <constantlescutprive2@gmail.com>
2026-05-05 12:38:02 +0100

    CloudinaryAPICODEDelete

--- src/backend/cloudinary.js
+++ src/backend/cloudinary.js
@@ -1,9 +1,7 @@
 import { v2 as cloudinary } from "cloudinary";
 
 cloudinary.config({
-  cloud_name: "dujvsxvat",
-  api_key: "837316735369859",
-  api_secret: "Hv6siMEbvurcCA5_5SvHDhWRpeQ"
+  ///////////////"
 });
 
 export default cloudinary;
