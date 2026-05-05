44121355871cf6bbcf17e9c70d280ba6175526c6
CRJL1001 <constantlescutprive2@gmail.com>
2026-05-05 12:37:35 +0100

    URLdelete

--- src/backend/db.js
+++ src/backend/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export async function connectDB(){
     try {
-        await mongoose.connect("mongodb+srv://admin:GeralD91365--@portfoliobase.hdqjx7y.mongodb.net/?appName=Portfoliobase");
+        await mongoose.connect("");
         console.log("connecté à MongoDB Atlas");
     }catch (err) {
         console.error("Erreur de connexion à MongoDB Atlas", err);
